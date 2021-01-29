eab5004b99a0682fd1e9cbc0ddb3244fcaa5d10c
Cantürk ATAŞ <atascanturk@gmail.com>
2021-01-29 18:31:46 +0300

    First Commit

---
+++ DictionaryStructure/MyDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryStructure
+{
+
+    // Here a dictionary was created with generics
+    // Key and Value can be any type
+
+    class MyDictionary<T1, T2>
+    {
+        T1[] index; // For Dictionary structure.
+        T2[] element;
+
+        public MyDictionary()
+        {
+            index = new T1[0]; // Defining an allocation for arrays.
+            element = new T2[0];
+        }
+
+        public void Add(T1 key, T2 value) 
+        {
+            T1[] tempIndex = index;
+            T2[] tempElement = element;
+
+            index = new T1[index.Length + 1];
+            element = new T2[element.Length + 1];
+
+            for (int i = 0; i < tempIndex.Length; i++)
+            {
+                index[i] = tempIndex[i];
+                element[i] = tempElement[i];
+            }
+
+            index[index.Length - 1] = key;
+            element[element.Length - 1] = value;
+
+        }
+
+        public int Length() 
+        {
+            return index.Length;
+        }
+
+    }
+}
---
+++ DictionaryStructure/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DictionaryStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+
+            Console.WriteLine("Please enter you key and value respesctively: ");
+            Console.WriteLine("Press '0' to exit.");
+
+            while (true)
+            {
+                int key;
+                string value;
+                key = Convert.ToInt32(Console.ReadLine());
+                if (key == 0)
+                {
+                    break;
+                }
+                value = Console.ReadLine();
+                myDictionary.Add(key, value);
+
+                break;
+            }
+            Console.WriteLine(myDictionary.Length()); // to control if it is working properly, or not?
+            
+        }
+    }
+}
